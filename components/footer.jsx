15549ab9f43d5477b2171ecf8035d59cbb1c9632
TEE2DWHY <isaacolorunfemi330@gmail.com>
2023-10-19 10:23:47 +0100

    made changes to footer.css

--- components/footer.jsx
+++ components/footer.jsx
@@ -6,10 +6,14 @@ const Footer = () => {
     <>
       <footer>
         <ul className="left">
-          <li>Support</li>
-          <li>Help Center</li>
-          <li>Privacy</li>
-          <li>Terms of Service</li>
+          <div className="left-container">
+            <li>Support</li>
+            <li>Help Center</li>
+          </div>
+          <div className="left-container">
+            <li>Privacy</li>
+            <li>Terms of Service</li>
+          </div>
         </ul>
         <ul className="right">
           <li> &copy; CopyRight - AppStack {year}</li>
