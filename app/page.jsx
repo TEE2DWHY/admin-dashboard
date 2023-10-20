92018a6302075edf34bd8a2cff851d7bf2d380fc
TEE2DWHY <isaacolorunfemi330@gmail.com>
2023-10-20 09:45:42 +0100

    made changes to pages.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Dashboard from "@/components/dashboard";
 import "../styles/home.css";
 import Sidebar from "@/components/sidebar";
@@ -7,18 +7,6 @@ import Animate from "@/libs/Animate";
 
 const Home = () => {
   const [isSidebarVisible, setSidebarVisible] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setSidebarVisible(window.innerWidth > 820);
-    };
-    if (typeof window !== "undefined") {
-      window.addEventListener("resize", handleResize);
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }
-  }, []);
 
   const toggleSidebar = () => {
     setSidebarVisible(!isSidebarVisible);
