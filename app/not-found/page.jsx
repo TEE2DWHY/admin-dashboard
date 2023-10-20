a0a2ea5503193d50cff21f5330c627b3ea3eab1f
TEE2DWHY <isaacolorunfemi330@gmail.com>
2023-10-20 14:39:21 +0100

    started working on auth pages

--- app/not-found/page.jsx
+++
@@ -1,5 +0,0 @@
-const custom404 = () => {
-  return <h1>NotFound Page</h1>;
-};
-
-export default custom404;
---
+++ app/sign-up/page.jsx
@@ -0,0 +1,41 @@
+// styling
+import "../../styles/auth.css";
+const SignUp = () => {
+  return (
+    <>
+      <div className="sign-up">
+        <div className="signup-header">
+          <h1>Get Started</h1>
+          <p>
+            Start creating the best possible user experience for you customers.
+          </p>
+        </div>
+        <form>
+          <div>
+            <label htmlFor="firstName">First Name </label>
+            <input type="text" placeholder="FirstName" required />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" placeholder="LastName" required />
+          </div>
+          <div>
+            <label htmlFor="email">Last Name</label>
+            <input type="email" placeholder="JohnDoe@gmail.com" required />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder="Password" required />
+          </div>
+          <div>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input type="password" placeholder="Confirm Password" required />
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
