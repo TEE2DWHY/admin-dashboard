290cd923ff34cde79c53cf9e3c8bc2896de8e022
TEE2DWHY <isaacolorunfemi330@gmail.com>
2023-10-23 12:00:26 +0100

    removed the api folder

--- api/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { login, signup } = require("../controllers/auth");
-
-router.post("/login", login);
-router.route("/signup").post(signup);
-
-module.exports = router;
